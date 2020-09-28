49e4b480e69d0303b8e64880d3b65d92075ab897
nashirba <ashernur@gmail.com>
2020-09-28 10:00:50 +0600

    :poop: failed to integrate python-telegram-bot to django

--- apps/bot/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ClickupUser, Message, TelegramUser
-
-admin.site.register(ClickupUser)
-admin.site.register(Message)
-admin.site.register(TelegramUser)
--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ClickupUser, Message, TelegramUser
 
-# Register your models here.
+
+admin.site.register(ClickupUser)
+admin.site.register(Message)
+admin.site.register(TelegramUser)
+
+'''
+@admin.register(ClickupUser)
+class ClickupUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('name')
+'''

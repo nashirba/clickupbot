7f07b8554a285d7a580bc53ec24a155b7e21ffa9
nashirba <ashernur@gmail.com>
2020-09-28 10:02:56 +0600

    :hammer: using django-telegrambot to integrate my bot into django all changes according to github/JungDev/django-telegrambot guide

--- apps/bot/urls.py
+++ apps/bot/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+# from django.views.decorators.csrf import csrf_exempt
+
 from .views import UserCodeRedirectView
 
 urlpatterns = [
-    path('', UserCodeRedirectView.as_view(), name='redirect_view'),
+    path('access_code/', UserCodeRedirectView.as_view(), name='redirect_view'),
+    # path('webhook/', csrf_exempt(BotView.as_view())),
+    path('webhook/', include('django_telegrambot.urls')),
+
 ]
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('access_code/', include('app.bot.urls')),
+    path('bot/', include('apps.bot.urls')),
 ]

4d0b19ae51f94cc234c4c8986735ff5b5ceba64b
nashirba <ashernur@gmail.com>
2020-09-30 12:16:10 +0600

    :underconstruction: creating another view for clickup updates

--- apps/bot/urls.py
+++ apps/bot/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import UserCodeRedirectView
+from .views import UserCodeRedirectView #, EventView
 
 urlpatterns = [
     path('access_code/', UserCodeRedirectView.as_view(), name='redirect_view'),
     path('webhook/', include('django_telegrambot.urls')),
+    # path('event/' EventView.as_view, name='event_view')
 ]
--- apps/bot/views.py
+++ apps/bot/views.py
@@ -1,12 +1,26 @@
+import json
 import requests
 
 from django.conf import settings
+from django.http import JsonResponse
+from django.views import View
 from django.views.generic.base import RedirectView
 
 from .models import ClickupUser, Message, TelegramUser
 
+TELEGRAM_URL = 'https://api.telegram.org/bot'
 
-class UserCodeRedirectView(RedirectView):
+
+def send_message(message, chat_id):
+        data = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown",
+        }
+        response = requests.post(f"{TELEGRAM_URL}{settings.BOT_TOKEN}/sendMessage", data=data)
+
+
+class UserCodeRedirectView(RedirectView):    
 
     def get(self, request):
         try:
@@ -28,15 +42,7 @@ class UserCodeRedirectView(RedirectView):
 
             # lets notify user of susseccful get token action
             text = 'Your 🔑token has been successfully saved ✅ \nPlease type /start again'
-            data = {
-                "chat_id": tel_user.chat_id,
-                "text": text,
-                "parse_mode": "Markdown",
-                }
-            telegram_url = "https://api.telegram.org/bot"
-            response = requests.post(
-                f"{telegram_url}{settings.BOT_TOKEN}/sendMessage", data=data
-            )
+            send_message(text, tel_user.chat_id)
 
             # get user
             auth_user_url = 'https://api.clickup.com/api/v2/user'
@@ -54,3 +60,23 @@ class UserCodeRedirectView(RedirectView):
         return super().get(request)
 
     url = 'https://telegram.me/test_clickup_bot'
+
+
+# class EventView(View):
+
+#     def post(self, request, *args, **kwargs):
+#         t_data = json.loads(request.body)
+#         t_message = t_data["message"]
+#         t_chat = t_message["chat"]
+
+#         try:
+#             text = t_message["text"].strip().lower()
+#         except Exception as e:
+#             return JsonResponse({"ok": "POST request processed"})
+
+#         text = text.lstrip("/")
+#         chat = tb_tutorial_collection.find_one({"chat_id": t_chat["id"]})
+#         msg = "Unknown command"
+#         send_message(msg, t_chat["id"])
+
+#         return JsonResponse({"ok": "POST request processed"})
